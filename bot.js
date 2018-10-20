1fe912830850e51d192f8f61e80988797643b750
aosd <43905562+aosd@users.noreply.github.com>
2018-10-20 14:42:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('message', msg => {
+  if (msg.content === '<@307439268731551744>') {
+    msg.reply('AFK.');
+  }
+});
+
+
 client.user.setGame(`24 Hours,AFK`)
+
+
+
+client.login(process.env.BOT_TOKEN);
