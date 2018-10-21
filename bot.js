c311588583bbdc259828d01ef229fb50131795cc
aosd <43905562+aosd@users.noreply.github.com>
2018-10-21 19:28:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,31 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 
+const devs = ['440481882799603717'];
+const adminprefix = "-";
+client.on('message', message => {
 
-client.on('ready', () => { 
-  console.log(`Loggen in As ${client.user.username}`)
- client.user.setGame(`#Majd,TrixMC ${client.guilds.size} Guild's`, "https://twitch.tv/TrixMC")
- client.user.setStatus(`Idle`)
-});
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/حب بلا حدود");
+      message.channel.sendMessage(`**✅   ${argresult}**`)
+  }
+  });
+
 
 
 
