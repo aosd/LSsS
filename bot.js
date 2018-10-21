1b750800dbdabb7e61d19e90ae15efaefad173b7
aosd <43905562+aosd@users.noreply.github.com>
2018-10-21 18:47:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('message', msg => {
-  if (msg.content === '<@307439268731551744>') {
-    msg.reply('AFK.');
-  }
-});
 
 
 client.on('ready', () => {
